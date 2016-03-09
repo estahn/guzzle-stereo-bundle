4c2494c39e6d05ef34e87d209a312d15ff7c6072
Enrico Stahn <mail@enricostahn.com>
2016-03-10 03:10:40 +1100

    Change namespace

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Flamingo\Bundle\GuzzleStereoBundle\DependencyInjection;
+namespace EnricoStahn\Bundle\GuzzleStereoBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
